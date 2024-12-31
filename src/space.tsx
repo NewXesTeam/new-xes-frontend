ef5fde59fa60453931f15be019d9b7fa357eeb41
lrs2187 <liurongshuo2022@outlook.com>
2024-12-31 22:08:06 +0800

    💫feat: add space.index overview

--- src/space.tsx
+++ src/space.tsx
@@ -1,19 +1,86 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Tabs, Tab, Container, Stack } from 'react-bootstrap';
+import { Tabs, Tab, Container, Stack, Card } from 'react-bootstrap';
 import NavbarComponent from './components/Navbar.tsx';
 import { UserInfo } from './interfaces/user.ts';
-import { SpaceProfile } from './interfaces/space.ts';
-import { checkLoggedIn } from './utils.ts';
+import { SpaceProfile, SpaceIndex } from './interfaces/space.ts';
+import { checkLoggedIn, getWorkLink } from './utils.ts';
 import './styles/common.scss';
 
 const SpaceTabs = {
-    HomeTab: () => {
-        return (
-            <Container>
-                <h1>主页</h1>
-            </Container>
-        );
+    HomeTab: ({ userId }: { userId: string }) => {
+        const OverviewItemCard = ({ title, value }: { title: string; value: number }) => {
+            return (
+                <Card border="dark" className="m-auto" body>
+                    {title}：
+                    <br />
+                    <span style={{ fontSize: '24px' }}>{value}</span>
+                </Card>
+            );
+        };
+
+        const [pageComponent, setPageComponent] = React.useState<React.JSX.Element>(<h2>加载中...</h2>);
+
+        React.useEffect(() => {
+            let ignore = false;
+
+            const func = async () => {
+                const response = await fetch(`/api/space/index?user_id=${userId}`);
+                const responseData: SpaceIndex = await response.json();
+
+                setPageComponent(
+                    <>
+                        <Card>
+                            <Card.Header>
+                                <h4>Ta 的成就</h4>
+                            </Card.Header>
+                            <Card.Body>
+                                <Stack direction="horizontal">
+                                    <OverviewItemCard title="作品总数" value={responseData.data.overview.works} />
+                                    <OverviewItemCard title="被点赞总数" value={responseData.data.overview.likes} />
+                                    <OverviewItemCard title="被浏览总数" value={responseData.data.overview.views} />
+                                    <OverviewItemCard title="被改编总数" value={responseData.data.overview.source_code_views} />
+                                    <OverviewItemCard title="被收藏总数" value={responseData.data.overview.favorites} />
+                                
+                                    <Card border="dark" className="m-auto" body>
+                                        代表作：
+                                        <br />
+                                        {responseData.data.representative_work ? (
+                                            <Card>
+                                                <Card.Header>
+                                                    <a
+                                                        href={getWorkLink(responseData.data.representative_work)}
+                                                        className="stretched-link"
+                                                    >
+                                                        {responseData.data.representative_work.name}
+                                                    </a>
+                                                </Card.Header>
+                                                <Card.Body className="py-0">
+                                                    <img
+                                                        src={responseData.data.representative_work.thumbnail}
+                                                        height={138}
+                                                        className="m-auto"
+                                                    />
+                                                </Card.Body>
+                                            </Card>
+                                        ) : (
+                                            '暂无代表作'
+                                        )}
+                                    </Card>
+                                </Stack>
+                            </Card.Body>
+                        </Card>
+                    </>,
+                );
+            };
+
+            if (!ignore) func();
+            return () => {
+                ignore = true;
+            };
+        }, []);
+
+        return <Container className="mt-2">{pageComponent}</Container>;
     },
     CoverTab: () => {
         return (
@@ -70,6 +137,8 @@ const SpacePage = () => {
                 ignore = true;
             };
         }, []);
+
+        return <div />; // 额，这是为了避免用户看到提示语感觉不舒服所以才不加任何内容
     }
 
     const [username, setUsername] = React.useState('Loading...');
@@ -112,19 +181,19 @@ const SpacePage = () => {
             </Stack>
 
             <Tabs className="mt-5 justify-content-center" transition={false}>
-                <Tab eventKey="home" title="主页">
-                    <SpaceTabs.HomeTab />
+                <Tab eventKey="home" title="主页" mountOnEnter unmountOnExit>
+                    <SpaceTabs.HomeTab userId={userId} />
                 </Tab>
-                <Tab eventKey="cover" title="封面">
+                <Tab eventKey="cover" title="封面" mountOnEnter unmountOnExit>
                     <SpaceTabs.CoverTab />
                 </Tab>
-                <Tab eventKey="projects" title="作品">
+                <Tab eventKey="projects" title="作品" mountOnEnter unmountOnExit>
                     <SpaceTabs.ProjectsTab />
                 </Tab>
-                <Tab eventKey="favorites" title="收藏">
+                <Tab eventKey="favorites" title="收藏" mountOnEnter unmountOnExit>
                     <SpaceTabs.FavoritesTab />
                 </Tab>
-                <Tab eventKey="fans" title="社交">
+                <Tab eventKey="fans" title="社交" mountOnEnter unmountOnExit>
                     <SpaceTabs.FansTab />
                 </Tab>
                 {/* 不打算支持垃圾勋章 */}
