5a9184830fc8e5d47a552bc34c679077ef876468
lrs2187 <liurongshuo2022@outlook.com>
2025-01-04 21:48:04 +0800

    💫feat: using container in index

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import { Work } from './interfaces/work.ts';
 
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Container } from 'react-bootstrap';
 import NavbarComponent from './components/Navbar.tsx';
 import WorkList from './components/WorkList.tsx';
 
@@ -31,36 +31,39 @@ const IndexPage = () => {
     return (
         <>
             <NavbarComponent />
-            <Carousel className="m-auto mt-5 mb-5" interval={3500}>
-                <Carousel.Item>
-                    <a href="https://code.xueersi.com/home/project/detail?lang=scratch&pid=24831951&version=3.0&langType=scratch">
-                        <img
-                            src="https://livefile.xesimg.com/programme/python_assets/2408b27e9c45a11098ddfd851844c4f1.png"
-                            className="d-block h-25"
-                            alt="ill"
-                        />
-                    </a>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <a href="https://code.xueersi.com/search">
-                        <img
-                            src="https://static0.xesimg.com/talcode/assets/home/banner/search_default.png"
-                            className="d-block h-25"
-                            alt="search"
-                        />
-                    </a>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <a href="https://code.xueersi.com/event">
-                        <img
-                            src="https://static0.xesimg.com/talcode/assets/home/banner/event_default.png"
-                            className="d-block h-25"
-                            alt="events"
-                        />
-                    </a>
-                </Carousel.Item>
-            </Carousel>
-            {cards}
+
+            <Container>
+                <Carousel className="m-auto mt-5 mb-5" interval={3500}>
+                    <Carousel.Item>
+                        <a href="https://code.xueersi.com/home/project/detail?lang=scratch&pid=24831951&version=3.0&langType=scratch">
+                            <img
+                                src="https://livefile.xesimg.com/programme/python_assets/2408b27e9c45a11098ddfd851844c4f1.png"
+                                className="d-block h-25"
+                                alt="ill"
+                            />
+                        </a>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <a href="https://code.xueersi.com/search">
+                            <img
+                                src="https://static0.xesimg.com/talcode/assets/home/banner/search_default.png"
+                                className="d-block h-25"
+                                alt="search"
+                            />
+                        </a>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <a href="https://code.xueersi.com/event">
+                            <img
+                                src="https://static0.xesimg.com/talcode/assets/home/banner/event_default.png"
+                                className="d-block h-25"
+                                alt="events"
+                            />
+                        </a>
+                    </Carousel.Item>
+                </Carousel>
+                {cards}
+            </Container>
         </>
     );
 };
