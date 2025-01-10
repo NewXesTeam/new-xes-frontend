ab2dccd0734d46cb32646e4c68485a959bb0c5d1
chen3283891376 <3283891376@qq.com>
2025-01-10 19:58:00 +0800

    Update UserList.tsx

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import { HorizontalUserCard, SmallUserCard } from './UserCard.tsx';
-import { SimpleUserInfo } from '../interfaces/user.ts';
-import { Row, Col, Stack } from 'react-bootstrap';
+import { Work } from '../interfaces/work.ts';
+import { Row, Col } from 'react-bootstrap';
+import WorkCard from './WorkCard.tsx';
+import { RemovedWorkCard } from './WorkCard.tsx';
 
-const UserVerticalList = ({ users }: { users: SimpleUserInfo[] }) => {
-    const cards = users.map((user, index) => <HorizontalUserCard key={user.id} className={index >= 1 ? 'mt-2' : ''} user={user} />);
+const WorkList = ({ works, className = '' }: { works: Work[]; className?: string }) => {
+    const cards = works.map((work: Work) => {
+        if (work.removed) {
+            return (
+                <Col key={work.topic_id}>
+                    <RemovedWorkCard />
+                </Col>
+            );
+        }
+        return (
+            <Col key={work.id}>
+                <WorkCard work={work} />
+            </Col>
+        );
+    });
 
-    return <Stack>{cards}</Stack>;
+    return (
+        <Row xs={1} sm={2} md={3} lg={4} xl={5} className={className}>
+            {cards}
+        </Row>
+    );
 };
 
-const UserHorizontalList = ({ users }: { users: SimpleUserInfo[] }) => {
-    const cards = users.map(user => (
-        <Col key={user.id}>
-            <SmallUserCard user={user} />
-        </Col>
-    ));
-
-    return <Row xs="auto">{cards}</Row>;
-};
-
-export { UserHorizontalList };
+export default WorkList;
