1a2be5a4ef06ba7788e9a3d43bdb3713dcd7cfcf
lrs2187 <liurongshuo2022@outlook.com>
2025-01-10 21:35:09 +0800

    💫feat: Navbar 添加头像显示

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown, Form } from 'react-bootstrap';
 import { checkLoggedIn } from '../utils.ts';
+import Avatar from './Avatar.tsx';
+import { UserInfo } from '../interfaces/user';
 
 const NavbarComponent = () => {
     const logoutEvent = async () => {
         await fetch('/passport/logout');
         location.reload();
     };
+    const [userName, setUserName] = React.useState<string>('');
+    const [userAvatar, setUserAvatar] = React.useState<string>('');
     let userComponent: React.JSX.Element;
 
     if (checkLoggedIn()) {
+        React.useEffect(() => {
+            let ignore = false;
+            const func = async () => {
+                const response = await fetch('/api/user/info');
+                const responseData: UserInfo = await response.json();
+                setUserName(responseData.data.name);
+                setUserAvatar(responseData.data.avatar_path);
+            };
+
+            if (!ignore) func();
+            return () => {
+                ignore = true;
+            };
+        }, []);
         userComponent = (
-            <NavDropdown title="用户" align={'end'}>
+            <NavDropdown title={<Avatar name={userName} avatarUrl={userAvatar} size={40} />} align={'end'}>
                 <NavDropdown.Item href="/space.html" target="_blank">
                     个人空间
                 </NavDropdown.Item>
@@ -30,17 +48,17 @@ const NavbarComponent = () => {
         <Navbar expand="lg" className="bg-body-tertiary shadow">
             <Container>
                 <Navbar.Brand href="/">
-                    <img src={require('../static/logo.png')} width={190} height={37}></img>
+                    <img src={require('../static/logo.png')} width={190} height={37} alt="logo"></img>
                 </Navbar.Brand>
 
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Collapse>
                     <Nav className="me-auto">
                         <Nav.Link href="/">主页</Nav.Link>
                         <Nav.Link href="/discover.html">发现</Nav.Link>
                         <Nav.Link href="/about.html">关于</Nav.Link>
                     </Nav>
 
-                    <Nav className="ms-auto">
+                    <Nav className="ms-auto" style={{ alignItems: 'center' }}>
                         <Form role="search" action="https://code.xueersi.com/search-center" className="me-2">
                             <Form.Control type="search" placeholder="搜索" className=" mr-sm-2" name="keyword" />
                         </Form>
