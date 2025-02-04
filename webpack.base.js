40f8175757455beaa7d00a01fec8a461a969dc44
chen3283891376 <3283891376@qq.com>
2025-02-04 19:15:56 +0800

    🚩prettier: 格式化一下

--- webpack.base.js
+++ webpack.base.js
@@ -7,7 +7,18 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const entriesName = ['index', 'about', 'login', 'eula', 'space', 'userInfo', 'discover', 'user', 'message', 'embed/cpp'];
+const entriesName = [
+    'index',
+    'about',
+    'login',
+    'eula',
+    'space',
+    'userInfo',
+    'discover',
+    'user',
+    'message',
+    'embed/cpp',
+];
 
 let entries = {};
 let plugins = [new MiniCssExtractPlugin()];
