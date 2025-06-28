06931e0f7e1a4989ac0383599d9849b853f8e4c8
lrs2187 <liurongshuo2022@outlook.com>
2025-06-28 21:51:40 +0800

    💫feat: 使用 vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,50 @@ export default defineConfig({
             tsconfig: 'tsconfig.json',
         },
     },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://code.xueersi.com',
+                changeOrigin: true,
+                cookieDomainRewrite: '127.0.0.1',
+                rewrite: (path) => path.replace(/^\/api/, '/api'),
+            },
+            '/passport/captcha': {
+                target: 'https://passport.100tal.com',
+                changeOrigin: true,
+                cookieDomainRewrite: '127.0.0.1',
+                headers: {
+                    Referer: 'https://login.xueersi.com/',
+                },
+                rewrite: (path) => path.replace(/^\/passport\/captcha/, '/v1/web/captcha/get'),
+            },
+            '/passport/login': {
+                target: 'https://passport.100tal.com',
+                changeOrigin: true,
+                cookieDomainRewrite: '127.0.0.1',
+                headers: {
+                    Referer: 'https://login.xueersi.com/',
+                },
+                rewrite: (path) => path.replace(/^\/passport\/login/, '/v1/web/login/pwd'),
+            },
+            '/passport/get_token': {
+                target: 'https://login.xueersi.com',
+                changeOrigin: true,
+                cookieDomainRewrite: '127.0.0.1',
+                headers: {
+                    Referer: 'https://login.xueersi.com/',
+                },
+                rewrite: (path) => path.replace(/^\/passport\/get_token/, '/V1/Web/getToken'),
+            },
+            '/passport/logout': {
+                target: 'https://login.xueersi.com',
+                changeOrigin: true,
+                cookieDomainRewrite: '127.0.0.1',
+                headers: {
+                    Referer: 'https://login.xueersi.com/',
+                },
+                rewrite: (path) => path.replace(/^\/passport\/logout/, '/newLogin/logout'),
+            },
+        },
+    }
 });
