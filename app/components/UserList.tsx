5241c810d976909916c6d209c2b03aec10611421
chen3283891376 <3283891376@qq.com>
2025-07-03 12:24:32 +0800

    💫feat: UserCard使用MUI

--- app/components/UserList.tsx
+++ app/components/UserList.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
-import { Row, Col, Stack } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
+import { Stack } from '@mui/material';
 import { HorizontalUserCard, SmallUserCard } from './UserCard';
 
 import type { SimpleUserInfo } from '@/interfaces/user';
 
 const UserVerticalList = ({ users }: { users: SimpleUserInfo[] }) => {
     const cards = users.map((user, index) => (
-        <HorizontalUserCard key={user.id} className={index >= 1 ? 'mt-2' : ''} user={user} />
+        <HorizontalUserCard key={user.id} className={index >= 1 ? 'mt-2' : ''} user={user} children={null} />
     ));
 
-    return <Stack>{cards}</Stack>;
+    return <Stack direction="column" spacing={2}>{cards}</Stack>;
 };
 
 const UserHorizontalList = ({ users }: { users: SimpleUserInfo[] }) => {
