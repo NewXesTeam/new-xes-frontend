5c65e76a1e15d4408453d1ef9a6e89fdcc97dbc5
lrs2187 <liurongshuo2022@outlook.com>
2025-08-12 15:49:22 +0800

    feat: 调整 DemoPage 布局

--- src/pages/DemoPage.vue
+++ src/pages/DemoPage.vue
@@ -18,40 +18,38 @@ const handleTagsChange = (event: any) => {
 </script>
 
 <template>
-    <v-container>
-        <div class="flex flex-col gap-4">
-            <h2 style="font-size: 24px">v-video 视频播放器 (lab)</h2>
-            <v-video src="https://livefile.xesimg.com/programme/python_assets/bceab5f2e467497b410095ccedc7cbfe.mp4" />
+    <v-container class="flex flex-col gap-4">
+        <h2 style="font-size: 24px">v-video 视频播放器 (lab)</h2>
+        <v-video src="https://livefile.xesimg.com/programme/python_assets/bceab5f2e467497b410095ccedc7cbfe.mp4" />
 
-            <v-divider />
+        <v-divider />
 
-            <h2 style="font-size: 24px">v-otp-input 验证码输入框</h2>
-            <div>
-                <v-otp-input type="text" :length="4" class="max-w-fit mx-auto" />
-            </div>
-
-            <v-divider />
-
-            <h2 style="font-size: 24px">v-chip-group 纸片选择器</h2>
-            <v-chip-group>
-                <v-chip>Chip 1</v-chip>
-                <v-chip>Chip 2</v-chip>
-                <v-chip>Chip 3</v-chip>
-            </v-chip-group>
-
-            <v-divider />
-
-            <h2 style="font-size: 24px">tags 标签选择器 (tagify)</h2>
-            <span>Result: {{ tagText }}</span>
-            <Tagify
-                :settings="{
-                    whitelist: ['游戏', '动画', '故事', '模拟', '艺术', '教程', '其他'],
-                    placeholder: '添加标签',
-                    dropdown: { enabled: 0 },
-                }"
-                @change="handleTagsChange"
-            />
+        <h2 style="font-size: 24px">v-otp-input 验证码输入框</h2>
+        <div>
+            <v-otp-input type="text" :length="4" class="max-w-fit mx-auto" />
         </div>
+
+        <v-divider />
+
+        <h2 style="font-size: 24px">v-chip-group 纸片选择器</h2>
+        <v-chip-group>
+            <v-chip>Chip 1</v-chip>
+            <v-chip>Chip 2</v-chip>
+            <v-chip>Chip 3</v-chip>
+        </v-chip-group>
+
+        <v-divider />
+
+        <h2 style="font-size: 24px">tags 标签选择器 (tagify)</h2>
+        <span>Result: {{ tagText }}</span>
+        <Tagify
+            :settings="{
+                whitelist: ['游戏', '动画', '故事', '模拟', '艺术', '教程', '其他'],
+                placeholder: '添加标签',
+                dropdown: { enabled: 0 },
+            }"
+            @change="handleTagsChange"
+        />
     </v-container>
 </template>
 
