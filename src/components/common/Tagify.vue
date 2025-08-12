9a14f49d514797ec85959b44717c4e23b5a9a27a
lrs2187 <liurongshuo2022@outlook.com>
2025-08-12 15:26:56 +0800

    fix: 修正 Tagify.vue

--- src/components/common/Tagify.vue
+++ src/components/common/Tagify.vue
@@ -1,23 +1,24 @@
 ﻿<template v-once>
     <textarea v-if="mode === 'textarea'" />
-    <input v-else>
+    <input v-else />
 </template>
 
 <script>
-import Tagify from "@yaireo/tagify/dist/tagify.esm.js";
-import "@yaireo/tagify/dist/tagify.css";
+import Tagify from '@yaireo/tagify/dist/tagify.esm.js';
+import '@yaireo/tagify/dist/tagify.css';
 
 export default {
-    name: "Tags",
+    name: 'Tags',
+    emits: ['change'],
     props: {
         mode: String,
         settings: Object,
     },
     mounted() {
-        this.tagify = new Tagify(this.$el, this.settings)
+        this.tagify = new Tagify(this.$el, this.settings);
         this.tagify.off('change').on('change', event => {
-            this.$emit("change", event)
-        })
-    }
+            this.$emit('change', event);
+        });
+    },
 };
 </script>
