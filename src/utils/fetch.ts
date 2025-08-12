6e833d390085f04d1c4b3fe0a527bbf88726d52d
lrs2187 <liurongshuo2022@outlook.com>
2025-08-12 16:42:10 +0800

    feat: 优化 FetchState 但 webstorm 在 template 里貌似不认...

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,20 +1,39 @@
 ﻿import { computed, onMounted, ref } from 'vue';
 import { commonFetch } from './common.ts';
 
-interface FetchState<T> {
-    data: T | null;
-    completed: boolean;
-    error: boolean;
-    errorMessage: string;
+interface BaseFetchState<T> {
     resolve(info: T): void;
     reject(message: string): void;
 }
+
+interface PendingFetchState<T> extends BaseFetchState<T> {
+    data: T | null;
+    completed: false;
+    error: false;
+    errorMessage: null;
+}
+
+interface SuccessFetchState<T> extends BaseFetchState<T> {
+    data: T;
+    completed: true;
+    error: false;
+    errorMessage: null;
+}
+
+interface ErrorFetchState<T> extends BaseFetchState<T> {
+    data: T | null;
+    completed: true;
+    error: true;
+    errorMessage: string;
+}
+
+type FetchState<T> = PendingFetchState<T> | SuccessFetchState<T> | ErrorFetchState<T>;
 
 export function useFetchState<T>(initialize: T | null = null) {
     const completedRef = ref(false);
     const dataRef = ref<T | null>(initialize);
     const errorRef = ref(false);
-    const errorMessageRef = ref('');
+    const errorMessageRef = ref<string | null>(null);
 
     return computed(
         () =>
