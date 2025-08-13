9e75f75a6e71ca671d6ef5b8bc03167678f074c6
lrs2187 <liurongshuo2022@outlook.com>
2025-08-13 15:21:56 +0800

    feat: 适应space页面的变化

--- src/layouts/SpaceLayout.vue
+++ src/layouts/SpaceLayout.vue
@@ -111,6 +111,10 @@ watch(spaceTab, newTab => {
     });
 });
 
+watch(() => route.meta.space, newTab => {
+    spaceTab.value = (newTab || "home") as string;
+})
+
 watch(
     () => route.params.userId,
     () => {
