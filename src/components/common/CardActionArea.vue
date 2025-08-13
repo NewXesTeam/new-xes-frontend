934bff853959d488dcedbae08d2624135a760fee
lrs2187 <liurongshuo2022@outlook.com>
2025-08-13 17:36:02 +0800

    feat: 分离 CardActionArea

---
+++ src/components/common/CardActionArea.vue
@@ -0,0 +1,67 @@
+﻿<script setup lang="ts">
+const { href, rel, target } = defineProps<{ href: string; rel?: string; target?: string; }>();
+</script>
+
+<template>
+    <a class="flex flex-col card-action-area" v-ripple :href="href" :rel="rel" :target="target">
+        <div class="area-overlay" />
+        <slot />
+    </a>
+</template>
+
+<style>
+@reference "tailwindcss";
+
+.card-action-area {
+    @apply rounded;
+    position: relative;
+    outline: none;
+}
+
+.card-action-area > * {
+    z-index: 0;
+}
+
+.card-action-area > .v-card-title {
+    z-index: 2;
+}
+
+.card-action-area > .area-overlay {
+    @apply rounded;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: currentColor;
+    opacity: 0;
+    transition: opacity 0.2s ease-in-out;
+    z-index: 1;
+}
+
+.card-action-area:hover > .area-overlay {
+    opacity: calc(var(--v-hover-opacity) * var(--v-theme-overlay-multiplier));
+}
+
+.card-action-area:focus-visible > .area-overlay {
+    opacity: calc(var(--v-focus-opacity) * var(--v-theme-overlay-multiplier));
+}
+
+.card-action-area::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+    border: 2px solid currentColor;
+    border-radius: inherit;
+    opacity: 0;
+    transition: opacity 0.2s ease-in-out;
+}
+
+.card-action-area:focus-visible::after {
+    opacity: calc(0.25 * var(--v-theme-overlay-multiplier));
+}
+</style>
--- src/components/work/WorkCard.vue
+++ src/components/work/WorkCard.vue
@@ -2,6 +2,7 @@
 import { getWorkLink } from '@/utils/index.ts';
 import type { Work } from '@/types/work.ts';
 import { computed } from 'vue';
+import CardActionArea from '@/components/common/CardActionArea.vue';
 
 const { work } = defineProps<{ work: Work }>();
 const thumbnail = computed(
@@ -16,15 +17,13 @@ const onOpenAuthorHome = () => {
 
 <template>
     <v-card v-tooltip:top="work.created_at">
-        <a class="flex flex-col card-action-area" v-ripple :href="getWorkLink(work)" rel="noopener" target="_blank">
+        <CardActionArea :href="getWorkLink(work)" rel="noopener" target="_blank">
             <v-img class="mx-auto" style="width: 224px; height: 168px" draggable="false" :src="thumbnail" />
 
             <v-card-title v-tooltip:bottom="name">
                 {{ name }}
             </v-card-title>
-
-            <div class="area-overlay" />
-        </a>
+        </CardActionArea>
 
         <v-card-actions class="flex justify-between">
             <v-btn @click="onOpenAuthorHome" color="primary">
@@ -48,59 +47,4 @@ const onOpenAuthorHome = () => {
     </v-card>
 </template>
 
-<style scoped>
-@reference "tailwindcss";
-
-.card-action-area {
-    @apply rounded;
-    position: relative;
-    outline: none;
-}
-
-.card-action-area > * {
-    z-index: 0;
-}
-
-.card-action-area > .v-card-title {
-    z-index: 2;
-}
-
-.card-action-area > .area-overlay {
-    @apply rounded;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: currentColor;
-    opacity: 0;
-    transition: opacity 0.2s ease-in-out;
-    z-index: 1;
-}
-
-.card-action-area:hover > .area-overlay {
-    opacity: calc(var(--v-hover-opacity) * var(--v-theme-overlay-multiplier));
-}
-
-.card-action-area:focus-visible > .area-overlay {
-    opacity: calc(var(--v-focus-opacity) * var(--v-theme-overlay-multiplier));
-}
-
-.card-action-area::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    pointer-events: none;
-    border: 2px solid currentColor;
-    border-radius: inherit;
-    opacity: 0;
-    transition: opacity 0.2s ease-in-out;
-}
-
-.card-action-area:focus-visible::after {
-    opacity: calc(0.25 * var(--v-theme-overlay-multiplier));
-}
-</style>
+<style scoped></style>
