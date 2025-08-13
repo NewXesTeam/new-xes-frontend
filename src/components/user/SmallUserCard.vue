3fa2fc4a4d13a6a5028d311d8770bcc35afd1b00
lrs2187 <liurongshuo2022@outlook.com>
2025-08-13 16:05:44 +0800

    feat: 添加 SmallUserCard.vue

---
+++ src/components/user/SmallUserCard.vue
@@ -0,0 +1,24 @@
+﻿<script setup lang="ts">
+import type { SimpleUserInfo } from '@/types/user.ts';
+
+const { user } = defineProps<{ user: SimpleUserInfo; }>();
+const tooltip = `粉丝：${user.fans} 关注：${user.follows}`;
+</script>
+
+<template>
+    <v-card
+        v-tooltip:top="tooltip"
+        rel="noopener"
+        target="_blank"
+        :href="`/space/${user.id}/home`"
+    >
+        <div class="flex justify-center" style="padding: 4px;">
+            <v-avatar :size="80" :image="user.avatar_path" />
+        </div>
+        <v-card-title>
+            {{ user.realname }}
+        </v-card-title>
+    </v-card>
+</template>
+
+<style scoped></style>
---
+++ src/components/user/UserHorizontalList.vue
@@ -0,0 +1,14 @@
+﻿<script setup lang="ts">
+import type { SimpleUserInfo } from '@/types/user.ts';
+import SmallUserCard from '@/components/user/SmallUserCard.vue';
+
+const { users } = defineProps<{ users: SimpleUserInfo[]; }>();
+</script>
+
+<template>
+    <div class="flex mx-auto w-fit gap-2">
+        <SmallUserCard v-for="user of users" :user="user" />
+    </div>
+</template>
+
+<style scoped></style>
