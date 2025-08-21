faedb1b6664a162f1de48b62593c349e5a27b953
lrs2187 <liurongshuo2022@outlook.com>
2025-08-21 21:11:38 +0800

    fix!: 学某思全家似了，作品搞个 null 喵

--- src/components/work/WorkList.vue
+++ src/components/work/WorkList.vue
@@ -2,6 +2,7 @@
 import type { Work } from '@/types/work.ts';
 import WorkCard from '@/components/work/WorkCard.vue';
 import RemovedWorkCard from '@/components/work/RemovedWorkCard.vue';
+import { v4 as uuid } from 'uuid';
 
 interface Props {
     className?: string;
@@ -14,7 +15,8 @@ const { className = 'm-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-
 
 <template>
     <div :class="className">
-        <component v-for="work of works" :key="work.id" :is="work.removed ? RemovedWorkCard : WorkCard" :work="work" />
+        <!-- 先尝试 id，不存在可能是作品被下架了尝试 topic_id，还不存在就 tmd 是学某思搞的 null 作品 -->
+        <component v-for="work of works" :key="work?.id || work?.topic_id || uuid()" :is="work && !work.removed ? WorkCard : RemovedWorkCard" :work="work" />
     </div>
 </template>
 
