92e7b66bce0464603b72279a401abbd11d63f5a0
chen3283891376 <3283891376@qq.com>
2026-02-19 10:11:42 +0800

    faet: 主页面好辣

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -1,10 +1,30 @@
-<script lang="ts" setup>
+<script setup lang="ts">
+import WorkList from '@/components/work/WorkList.vue';
+import Loading from '@/components/common/Loading.vue';
+import { useAppStore } from '@/stores/app.ts';
+import { useFetchData } from '@/utils/index.ts';
+import type { Work } from '@/types/work.ts';
+import { onMounted } from 'vue';
 import Layout from '@/components/Layout.vue';
 
+const store = useAppStore();
+const [followsWorkData, loadFollowsWorkData] = useFetchData<Work[]>('/api/index/works/follows', {}, []);
+
+onMounted(() => {
+    loadFollowsWorkData();
+});
 </script>
 
 <template>
     <Layout>
-        awa
+            <h1 style="font-size: 36px" v-if="!store.isLoggedIn">欢迎来到 NewXesFrontend</h1>
+            <div class="flex flex-col gap-2" v-if="store.isLoggedIn">
+                <h2 style="font-size: 24px">我的关注</h2>
+                <v-divider />
+                <Loading v-if="!followsWorkData.success" :error="followsWorkData.error" />
+                <WorkList v-else :works="followsWorkData.data || []" />
+            </div>
     </Layout>
 </template>
+
+<style scoped></style>
